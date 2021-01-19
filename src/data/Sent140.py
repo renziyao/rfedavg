22ff13daee69ce7b7270cf2d08e3f483657e6d65
ZiyaoRen <2571476073@qq.com>
2021-01-19 14:59:35 +0800

    rewrite sent140

--- src/data/Sent140.py
+++ src/data/Sent140.py
@@ -9,6 +9,7 @@ from torchtext.experimental.functional import (
 )
 from torchtext.experimental.datasets.text_classification import TextClassificationDataset
 
+# data load
 df = pd.read_csv(
     "./data/training.1600000.processed.noemoticon.csv", 
     engine="python", 
@@ -24,26 +25,19 @@ def get_vocab_counter(data, transforms):
             counter[tokens] += 1
     return counter
 
-def non_iid_percent(params):
-    s = params['Dataset']['s']
+def cross_device_niid(params):
     num_user = params['Trainer']['n_clients']
-    pick_need = 50000
-    pick_sum = 0
-    usernames = []
-    for k, v in dict(df[4].value_counts()).items():
-        pick_sum += v
-        usernames.append(k)
-        if pick_sum > pick_need: break
+    dataset_user = params['Dataset']['user']
+    assert num_user == dataset_user # should be exact same
+    usernames = list(dict(df[4].value_counts()))[:dataset_user]
     df_small = df.loc[df[4].isin(usernames)]
-    df_small = (df_small.iloc[:pick_need, :]).sample(frac=1)
-    df_train = (df_small.iloc[:int(pick_need * 0.8), :])
-    df_test = df_small.iloc[int(pick_need * 0.8):, :]
-
+    df_small = df_small.sample(frac=1) # shuffle all the data
+    df_train = df_small.iloc[:int(df_small.shape[0] * 0.9), :]
+    df_test = df_small.iloc[int(df_small.shape[0] * 0.9):, :]
     text_transform = sequential_transforms(
         str.lower, 
         get_tokenizer("basic_english"),
     )
-
     counter = Counter(dict(
         get_vocab_counter(df_train[5], text_transform).most_common(3000 - 2)
     ))
@@ -52,26 +46,134 @@ def non_iid_percent(params):
         vectors='glove.6B.300d', 
         vectors_cache='./data/vector_cache/',
     )
-
     text_transform = sequential_transforms(
         text_transform, 
         vocab_func(vocab), 
         totensor(dtype=torch.long), 
     )
     label_transform = sequential_transforms(totensor(dtype=torch.long))
-
-    # pandas is easy to split
-    #data_train = list(zip(df_train[0], df_train[5]))
-    #train_dataset = TextClassificationDataset(data_train, vocab, (label_transform, text_transform))
     data_test = list(zip(df_test[0], df_test[5]))
     test_dataset = TextClassificationDataset(
         data_test, 
         vocab, 
         (label_transform, text_transform),
     )
+    # pandas is easy to split
+    #data_train = list(zip(df_train[0], df_train[5]))
+    #train_dataset = TextClassificationDataset(data_train, vocab, (label_transform, text_transform))
+    dataset_split = []
+    for username in usernames:
+        split_train = df_small.loc[df_small[4] == username]
+        split_train = list(zip(split_train[0], split_train[5]))
+        dataset_split.append(
+            {
+                'train': TextClassificationDataset(
+                    split_train, 
+                    vocab, 
+                    (label_transform, text_transform),
+                ),
+                'test': None, 
+            }
+        )
+    return dataset_split, test_dataset
 
+def cross_device_iid(params):
+    num_user = params['Trainer']['n_clients']
+    dataset_user = params['Dataset']['user']
+    usernames = list(dict(df[4].value_counts()))[:dataset_user]
+    df_small = df.loc[df[4].isin(usernames)]
+    df_small = df_small.sample(frac=1) # shuffle all the data
+    df_train = df_small.iloc[:int(df_small.shape[0] * 0.9), :]
+    df_test = df_small.iloc[int(df_small.shape[0] * 0.9):, :]
+    text_transform = sequential_transforms(
+        str.lower, 
+        get_tokenizer("basic_english"),
+    )
+    counter = Counter(dict(
+        get_vocab_counter(df_train[5], text_transform).most_common(3000 - 2)
+    ))
+    vocab = Vocab(
+        counter, 
+        vectors='glove.6B.300d', 
+        vectors_cache='./data/vector_cache/',
+    )
+    text_transform = sequential_transforms(
+        text_transform, 
+        vocab_func(vocab), 
+        totensor(dtype=torch.long), 
+    )
+    label_transform = sequential_transforms(totensor(dtype=torch.long))
+    data_test = list(zip(df_test[0], df_test[5]))
+    test_dataset = TextClassificationDataset(
+        data_test, 
+        vocab, 
+        (label_transform, text_transform),
+    )
+    # pandas is easy to split
+    #data_train = list(zip(df_train[0], df_train[5]))
+    #train_dataset = TextClassificationDataset(data_train, vocab, (label_transform, text_transform))
+    df_train_iid = df_train.sample(frac=1)
+    p_train_iid = 0
+    delta_train_iid = df_train_iid.shape[0] // num_user
+    dataset_split = []
+    for userid in range(num_user):
+        train_lst = []
+        split_train = df_train_iid[
+            p_train_iid: p_train_iid + delta_train_iid
+        ]
+        print(split_train)
+        split_train = list(zip(split_train[0], split_train[5]))
+        dataset_split.append(
+            {
+                'train': TextClassificationDataset(
+                    split_train, 
+                    vocab, 
+                    (label_transform, text_transform),
+                ),
+                'test': None, 
+            }
+        )
+        p_train_iid += delta_train_iid
+    return dataset_split, test_dataset
+
+def cross_silo(params):
+    num_user = params['Trainer']['n_clients']
+    dataset_user = params['Dataset']['user']
+    s = params['Dataset']['s']
+    usernames = list(dict(df[4].value_counts()))[:dataset_user]
+    df_small = df.loc[df[4].isin(usernames)]
+    df_small = df_small.sample(frac=1) # shuffle all the data
+    df_train = df_small.iloc[:int(df_small.shape[0] * 0.9), :]
+    df_test = df_small.iloc[int(df_small.shape[0] * 0.9):, :]
+    text_transform = sequential_transforms(
+        str.lower, 
+        get_tokenizer("basic_english"),
+    )
+    counter = Counter(dict(
+        get_vocab_counter(df_train[5], text_transform).most_common(3000 - 2)
+    ))
+    vocab = Vocab(
+        counter, 
+        vectors='glove.6B.300d', 
+        vectors_cache='./data/vector_cache/',
+    )
+    text_transform = sequential_transforms(
+        text_transform, 
+        vocab_func(vocab), 
+        totensor(dtype=torch.long), 
+    )
+    label_transform = sequential_transforms(totensor(dtype=torch.long))
+    data_test = list(zip(df_test[0], df_test[5]))
+    test_dataset = TextClassificationDataset(
+        data_test, 
+        vocab, 
+        (label_transform, text_transform),
+    )
+    # pandas is easy to split
+    #data_train = list(zip(df_train[0], df_train[5]))
+    #train_dataset = TextClassificationDataset(data_train, vocab, (label_transform, text_transform))
     df_train_iid = df_train.iloc[:int(s * df_train.shape[0]), :]
-    df_train_niid = df_train.iloc[int(s * df_train.shape[0]):, :].sort_values([4])
+    df_train_niid = df_train.iloc[int(s * df_train.shape[0]):, :].sort_values([0])
     p_train_iid = 0
     p_train_niid = 0
     delta_train_iid = df_train_iid.shape[0] // num_user
